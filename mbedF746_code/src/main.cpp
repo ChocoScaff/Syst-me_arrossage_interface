d5f9e89c0d5da1e70cb41465635a91cc85875855
BROUZES ANTOINE <antoine.brouzes@geii2.iut-cachan.u-psud.fr>
2023-06-06 17:24:12 +0200

    --Add DHT library

--- mbedF746_code/src/main.cpp
+++ mbedF746_code/src/main.cpp
@@ -4,19 +4,24 @@
 #//include "demos/lv_demos.h"
 #include <cstdio>
 //#include <time.h>
+#include "DHT.h"
 
 ThreadLvgl threadLvgl(30);
 
+static lv_style_t style_btn;
+static lv_style_t style_btn_pressed;
+static lv_style_t style_btn_red;
+
+DHT dht(A0,DHT22);
 PwmOut pwmout(D1);
 
 int main() {
 
     threadLvgl.lock();
-
+    
     //lv_demo_widgets();
 
     threadLvgl.unlock();
-
 
     pwmout.period_ms(10);
     pwmout.write(0.4f);
@@ -25,7 +30,10 @@ int main() {
 
     while (1) {
         //digitalin.read();
-        
+        if(dht.readData() == ERROR_NONE) {
+ 
+
+        }        
         // put your main code here, to run repeatedly:
         ThisThread::sleep_for(10ms);
     }
